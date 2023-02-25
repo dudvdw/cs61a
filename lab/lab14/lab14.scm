934455ca5209fbf2de858ad88ca5ca6b7cfb2a4b
dudvdw <dudvdw@163.com>
2023-02-25 10:13:38 +0800

    lab14 scheme

--- lab/lab14/lab14.scm
+++ lab/lab14/lab14.scm
@@ -1,9 +1,23 @@
 (define (split-at lst n)
-  
+  (cond
+    ((> n (length lst)) (cons lst nil))
+    ((zero? n) (cons nil lst))
+    (else
+      (cons (cons (car lst) (car (split-at (cdr lst) (- n 1)))) (cdr (split-at (cdr lst) (- n 1))))
+  ))
 )
 
 
 (define (compose-all funcs)
   'YOUR-CODE-HERE
+  (define (compose x)
+    (cond
+      ((null? funcs) x)
+      (else 
+        ((compose-all (cdr funcs)) ((car funcs) x))
+      )
+    )
+  )
+  compose
 )
 
